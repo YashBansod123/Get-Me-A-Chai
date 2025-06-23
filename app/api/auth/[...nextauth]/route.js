4c4ca07beea39978fc70687fc0f43ecce04e94c1
YashBansod123 <144675161+YashBansod123@users.noreply.github.com>
2025-06-23 21:30:11 +0530

    again

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,7 @@ import User from '@/models/User';
 import Payment from '@/models/Payment';
  
 
-export const authoptions =  NextAuth({
+const authoptions =  NextAuth({
     providers: [
       // OAuth authentication providers...
       GitHubProvider({
